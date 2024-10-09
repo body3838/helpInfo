dc1195b874fe82cf1149cdf701af86f2f0d239aa
body3838 <ck8b@proton.me>
2024-10-09 14:21:35 +0000

    ajout évaluation mémoire libre

--- helpInfo.sh
+++ helpInfo.sh
@@ -1,2 +1,4 @@
 echo "#### Linux Version"
 lsb_release -d | cut -f2- || cat /etc/os-release | grep PRETTY_NAME | cut -d '"' -f2
+echo -e "\n#### Free Memory"
+free -h | grep "Mem:" | awk '{print $4}'
